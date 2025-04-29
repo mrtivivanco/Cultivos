28e0c49531c3aace467ad9c29c17730656439e53
Vicho222 <vicemartinez@alumnos.uai.cl>
2025-04-29 14:18:28 -0400

    avance

--- Codigo_fuente/Interfaz/MenuPrincipal.java
+++ Codigo_fuente/Interfaz/MenuPrincipal.java
@@ -15,3 +15,12 @@ public class MenuPrincipal {
         this.gestorCultivos = new GestorDeCultivos();
         this.gestorParcelas = new GestorDeParcelas();
     }
+
+    private void mostrarMenu() {
+        System.out.println("\n=== MENÚ PRINCIPAL ===");
+        System.out.println("1. Listar cultivos");
+        System.out.println("2. Crear nuevo cultivo");
+        System.out.println("3. Eliminar cultivo");
+        System.out.println("4. Editar cultivo");
+        System.out.println("5. Listar parcelas");
+        System.out.println("6. Agregar parcela");
