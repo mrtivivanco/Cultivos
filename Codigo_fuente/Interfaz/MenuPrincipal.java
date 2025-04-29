c718c1ade6f3478a1a082f6b45b58264aa1bc2b4
Vicho222 <vicemartinez@alumnos.uai.cl>
2025-04-29 14:12:20 -0400

    avance menu principal app

--- Codigo_fuente/Interfaz/MenuPrincipal.java
+++ Codigo_fuente/Interfaz/MenuPrincipal.java
@@ -1,6 +1,17 @@
 package Codigo_fuente.Interfaz;
 
+import java.util.Scanner;
+import services.GestorDeCultivos;
+import services.GestorDeParcelas;
+
 public class MenuPrincipal {
-    
-}
-//Aquí estará el menú y la interacción con el usuario
+
+    private final Scanner scanner;
+    private final GestorDeCultivos gestorCultivos;
+    private final GestorDeParcelas gestorParcelas;
+
+    public MenuPrincipal() {
+        this.scanner = new Scanner(System.in);
+        this.gestorCultivos = new GestorDeCultivos();
+        this.gestorParcelas = new GestorDeParcelas();
+    }
