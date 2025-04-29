6beb85903c44d2106752866dd4e728b3dbbb380b
Vicho222 <vicemartinez@alumnos.uai.cl>
2025-04-29 14:28:27 -0400

    opciones menu

--- Codigo_fuente/Interfaz/MenuPrincipal.java
+++ Codigo_fuente/Interfaz/MenuPrincipal.java
@@ -16,11 +16,44 @@ public class MenuPrincipal {
         this.gestorParcelas = new GestorDeParcelas();
     }
 
-    private void mostrarMenu() {
-        System.out.println("\n=== MENÚ PRINCIPAL ===");
-        System.out.println("1. Listar cultivos");
-        System.out.println("2. Crear nuevo cultivo");
-        System.out.println("3. Eliminar cultivo");
-        System.out.println("4. Editar cultivo");
-        System.out.println("5. Listar parcelas");
-        System.out.println("6. Agregar parcela");
+    public void iniciar() {
+        int opcion;
+        do {
+            mostrarMenu();
+            System.out.print("Seleccione una opción: ");
+            opcion = Integer.parseInt(scanner.nextLine());
+
+            switch (opcion) {
+                case 1:
+                    gestorCultivos.listarCultivos();
+                    break;
+                case 2:
+                    gestorCultivos.crearCultivo();
+                    break;
+                case 3:
+                    gestorCultivos.eliminarCultivo();
+                    break;
+                case 4:
+                    gestorCultivos.editarCultivo();
+                    break;
+                case 5:
+                    gestorParcelas.listarParcelas();
+                    break;
+                case 6:
+                    gestorParcelas.agregarParcela();
+                    break;
+                case 7:
+                    gestorParcelas.eliminarParcela();
+                    break;
+                case 8:
+                    gestorParcelas.editarParcela();
+                    break;
+                case 9:
+                    System.out.println("Saliendo y guardando en CSV...");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+
+        } while (opcion != 9);
+    }
