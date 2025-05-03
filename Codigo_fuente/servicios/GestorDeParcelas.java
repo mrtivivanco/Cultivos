476f90db1bfedf1c593738e8d5833865d9a43c3e
mane-lab <mane.ulloab@gmail.com>
2025-05-03 12:19:59 -0400

    Update GestorDeParcelas.java

--- Codigo_fuente/servicios/GestorDeParcelas.java
+++ Codigo_fuente/servicios/GestorDeParcelas.java
@@ -1,21 +1,61 @@
 package Codigo_fuente.servicios;
 
+import Codigo_fuente.clases.Parcela;
+import Codigo_fuente.clases.Cultivo;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-import Codigo_fuente.clases.Parcela;
-
+//Esta clase se encargará de gestionar las parcelas
 public class GestorDeParcelas {
-	
-	List<Parcela> listaParcelas = new ArrayList<>();
 
-	public List<Parcela> getListaParcelas() {
-		return listaParcelas;
+	//Creamos una lista que almacenará las parcelas creadas
+	private List<Parcela> listaParcelas;
+
+	//Constructor: se ejecuta al crear un objeto de tipo GestorDeParcelas
+	public GestorDeParcelas() {
+	        listaParcelas = new ArrayList<>(); //Inicializamos la lista vacía
 	}
 
-	public void setListaParcelas(List<Parcela> listaParcelas) {
-		this.listaParcelas = listaParcelas;
-	}
+	// Método para agregar parcela
+        public boolean agregarParcela(Scanner scanner) {
+        System.out.println("\n=== Agregar Nueva Parcela ===");
+        System.out.print("Ingrese código de parcela: ");
+        String codigo = scanner.nextLine();
+        
+        if(buscarParcela(codigo) != null) {
+            System.out.println("¡Esta parcela ya existe!");
+            return false;
+        }
+        
+        Parcela nueva = new Parcela(codigo);
+        listaParcelas.add(nueva);
+        return true;
+    }
+
+    // Método para listar parcelas
+    public void listarParcelas() {
+        if(listaParcelas.isEmpty()) {
+            System.out.println("\nNo hay parcelas registradas");
+            return;
+        }
+
+        System.out.println("\n=== Listado de Parcelas ===");
+        for(Parcela p : listaParcelas) {
+            System.out.println("Parcela: " + p.getCodigoParcela());
+            System.out.println("Cultivos asociados: " + p.getListaCultivos().size());
+            System.out.println("----------------------");
+        }
+    }
+
+    // Getters y Setters
+    public List<Parcela> getListaParcelas() {
+        return listaParcelas;
+    }
+
+    public void setListaParcelas(List<Parcela> listaParcelas) {
+        this.listaParcelas = listaParcelas;
+    }
     
 }
-//Aquí irá la lógica del programa (leer CSV, guardar datos, etc.
